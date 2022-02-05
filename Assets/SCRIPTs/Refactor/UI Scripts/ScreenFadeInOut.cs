ab5c6ca7edbdb6daf9b120a39081c52242f7d05f
Sanjar Abrorxojaev <sabrorxojaev@gmail.com>
2022-02-05 18:35:39 +0500

    Add DoTween | Fix

--- Assets/SCRIPTs/Refactor/UI Scripts/ScreenFadeInOut.cs
+++ Assets/SCRIPTs/Refactor/UI Scripts/ScreenFadeInOut.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class ScreenFadeInOut : MonoBehaviour
 {
@@ -28,17 +29,9 @@ public class ScreenFadeInOut : MonoBehaviour
         yield return new WaitForSeconds(1f);
 
         _fade.gameObject.SetActive(true);
-        for (float i = 0; i < 270; i += 5)
-        {
-            _fade.color = new Color(_fade.color.r, _fade.color.g, _fade.color.b, i / 255);
-            yield return new WaitForSeconds(0.01f);
-        }
-        yield return new WaitForSeconds(.5f);
-        for (float i = 270; i > 0; i -= 5)
-        {
-            _fade.color = new Color(_fade.color.r, _fade.color.g, _fade.color.b, i / 255);
-            yield return new WaitForSeconds(0.01f);
-        }
+        _fade.DOFade(1,.7f);
+        yield return new WaitForSeconds(1f);
+        _fade.DOFade(0 ,1f);
         _fade.gameObject.SetActive(false);
 
     }
